4b1347f11400652771921a1b0072e56f7a13f535
kaninkearpimy23814 <kanin23814@gmail.com>
2022-01-07 22:50:16 +0700

    complete palindrome

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+use std::io::stdin;
+
 fn main() {
     println!("Palindrome.");
+    println!("If you want to exit. please type ==> :q\n");
+    loop {
+        let mut pre_string = String::new();
+        println!("Please input your thing ==>");
+        stdin().read_line(&mut pre_string).expect("Please put correct string.");
+        
+        let string: &str = &pre_string.trim().replace(" ", "");
+        if string == ":q" {
+            println!("==========");
+            println!("THANK YOU.");
+            println!("==========");
+            break;
+        }
+        if string == "" {
+            println!("Please put correct string!\n");
+            continue;
+        }
 
-    let string = "anna";
+        let is_palindrome: bool = check_palindrom(string);
 
-    let is_palindrome: bool = check_palindrom(string);
-
-    println!("is {} Palindrome? : {}", string, is_palindrome);
+        println!("is {} Palindrome? : {}\n", string, match is_palindrome { true => "YES", false => "NO" });
+    }
 }
 
 fn check_palindrom(string: &str) -> bool {
